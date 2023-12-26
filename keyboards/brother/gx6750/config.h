2bf3f83c7e28c689c25cb94346685f9d325dd3c7
Juan Vergara <juan.e.vergara@hotmail.com>
2023-12-25 19:19:21 -0500

    Support for handwired Brother GX6750

---
+++ keyboards/brother/gx6750/config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include_next <config.h>
+
+// Vial
+#define VIAL_KEYBOARD_UID {0x40, 0xBF, 0x1F, 0xCB, 0xD1, 0x06, 0xC3, 0xEA}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+
+// Solenoid
+#define SOLENOID_ACTIVE true
+#define SOLENOID_DEFAULT_DWELL 10 
+#define SOLENOID_MAX_DWELL 20
+#define SOLENOID_MIN_DWELL 5
+#define SOLENOID_PIN GP16
+
+// 2KRO
+#define MATRIX_HAS_GHOST
+
+// Optimizations
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_PRINT
+//#define NO_DEBUG
---
+++ keyboards/brother/gx6750/rules.mk
@@ -0,0 +1,29 @@
+# This file intentionally left blank
+
+# Optimizations
+#LTO_ENABLE = yes
+#QMK_SETTINGS = no
+#COMBO_ENABLE = no
+#TAP_DANCE_ENABLE = no
+#DYNAMIC_MACRO_ENABLE = no
+
+#BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
+#MOUSEKEY_ENABLE = no       # Mouse keys
+#EXTRAKEY_ENABLE = yes       # Audio control and System control
+#CONSOLE_ENABLE = no        # Console for debug
+#COMMAND_ENABLE = no        # Commands for debug and configuration
+#NKRO_ENABLE = yes           # Enable N-Key Rollover
+#BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+#RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+#AUDIO_ENABLE = no           # Audio output
+#CUSTOM_MATRIX = yes
+
+# VIAL
+# VIAL_INSECURE = yes
+
+# Solenoid
+HAPTIC_ENABLE = yes
+HAPTIC_DRIVER = solenoid
+
+# Hardware?
+MCU = RP2040
